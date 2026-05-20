bf19695baee545e7fa1698f5b47cdb151fde9724
mishraamitkumar644 <69366648+mishraamitkumar644@users.noreply.github.com>
2026-05-20 17:55:31 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,7 +14,7 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "e397672c-2118-4f8c-918d-90f1bdb9bc73"
+  subscription_id = "e397652c-2118-4f8c-918d-90f1bdb9bc73"
 }
 
 # -----------------------------
