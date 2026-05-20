7522285877037a944c3bbc7e864bc2afe926937c
mishraamitkumar644 <69366648+mishraamitkumar644@users.noreply.github.com>
2026-05-20 10:21:28 +0000

    Enhance Terraform configuration for Azure resources
    Added SKU for public IP and updated SSH public key variable.

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,33 +7,21 @@ terraform {
   }
 }
 
-#-------------------------
-# PROVIDER
-#-------------------------
 provider "azurerm" {
   features {}
   subscription_id = "e397672c-2118-4f8c-918d-90f1bdb9bc73"
 }
 
-#-------------------------
-# DATA: CUSTOM IMAGE
-#-------------------------
 data "azurerm_image" "custom" {
   name                = "ubuntu-docker-nginx"
   resource_group_name = "rg-canada-prod"
 }
 
-#-------------------------
-# RESOURCE GROUP
-#-------------------------
 resource "azurerm_resource_group" "rg" {
   name     = "rg-vm-prod"
   location = "canadacentral"
 }
 
-#-------------------------
-# VIRTUAL NETWORK
-#-------------------------
 resource "azurerm_virtual_network" "vnet" {
   name                = "vm-vnet"
   location            = azurerm_resource_group.rg.location
@@ -41,9 +29,6 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = ["10.0.0.0/16"]
 }
 
-#-------------------------
-# SUBNET
-#-------------------------
 resource "azurerm_subnet" "subnet" {
   name                 = "vm-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
@@ -51,19 +36,14 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-#-------------------------
-# PUBLIC IP
-#-------------------------
 resource "azurerm_public_ip" "pip" {
   name                = "vm-ip"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static" # Dynamic is deprecated for many SKU types; Static is safer
+  allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
-#-------------------------
-# NETWORK INTERFACE
-#-------------------------
 resource "azurerm_network_interface" "nic" {
   name                = "vm-nic"
   location            = azurerm_resource_group.rg.location
@@ -77,12 +57,11 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
-#-------------------------
-# VARIABLES
-#-------------------------
 variable "ssh_public_key" {
   description = "SSH public key for VM access"
   type        = string
+  # REPLACE THE STRING BELOW WITH YOUR ACTUAL PUBLIC KEY
+  default     = "ssh-rsa YOUR_KEY_HERE" 
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
@@ -90,12 +69,8 @@ resource "azurerm_linux_virtual_machine" "vm" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   size                = "Standard_B2s"
-
-  admin_username = "azureuser"
-
-  network_interface_ids = [
-    azurerm_network_interface.nic.id
-  ]
+  admin_username      = "azureuser"
+  network_interface_ids = [azurerm_network_interface.nic.id]
 
   os_disk {
     caching              = "ReadWrite"
